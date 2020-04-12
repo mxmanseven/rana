740f041cbb47be4b3e273e9396109d79ee3e4d88
mxmanseven <karlhess@msn.com>
2020-04-11 18:14:13 -0700

    Added ionic native camera and social sharing. Updated Tab3 to match example.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 import { environment } from "../environments/environment";
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -20,6 +21,7 @@ import { AppComponent } from './app.component';
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
+    AngularFirestoreModule,
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
